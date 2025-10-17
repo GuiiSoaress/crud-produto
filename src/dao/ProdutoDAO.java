d0ec171de6691e2c2d9c91fdcdd2a05710e6ee2f
Guilherme Soares <guihhsoaress@gmail.com>
2025-10-16 22:49:42 -0300

    NEW: Classe ProdutoDAO (Data Access Object)

---
+++ src/dao/ProdutoDAO.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Produto;
+
+public class ProdutoDAO {
+    
+    // -----------------------------------------------------------
+    // READ 
+    // -----------------------------------------------------------
+
+    public list<Produto> buscarTodos(){
+
+        public List<Produto> produtos = new ArrayList<>();
+
+        String sql = "SELECT * FROM produtos";
+
+        try () {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+}
