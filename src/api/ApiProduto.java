3dc8208c21ce9b1ca809706162334b6826971eaf
Guilherme Soares <guihhsoaress@gmail.com>
2025-10-23 22:38:40 -0300

    NEW: API /produtos/id

--- src/api/ApiProduto.java
+++ src/api/ApiProduto.java
@@ -7,6 +7,7 @@ import static spark.Spark.port;
 import com.google.gson.Gson;
 
 import dao.ProdutoDAO;
+import model.Produto;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
@@ -30,10 +31,33 @@ public class ApiProduto {
             }
         });
 
+        // GET /produtos - Buscar Todos
         get("/produtos", new Route(){
             @Override
             public Object handle(Request request, Response response){
                 return gson.toJson(dao.buscarTodos());
+            }
+        });
+
+        //GET /produtos/id
+        get("/produtos/:id", new Route(){
+            @Override
+            public Object handle(Request request, Response response){
+                try {
+                    Long id = Long.parseLong(request.params(":id"));
+
+                    Produto produto = dao.buscarPorId(id);
+
+                    if(produto != null){
+                        return gson.toJson(produto);
+                    }else{
+                        response.status(404);
+                        return "{\"mensagem\": \"Produto com ID " + id + "  Não encontrado\"}";
+                    }
+                } catch (NumberFormatException e) {
+                    response.status(400);
+                    return "{\"mensagem\": \"Id Invalido\"}";
+                }
             }
         });
 
