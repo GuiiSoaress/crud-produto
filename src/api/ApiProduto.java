ca7ee1545dad1c63299cc98f662164c86ae8cf26
Guilherme Soares <guihhsoaress@gmail.com>
2025-11-06 22:48:36 -0300

    NEW: Classe CategoriaDAO e APIs das categorias em ApiProduto

--- src/api/ApiProduto.java
+++ src/api/ApiProduto.java
@@ -9,16 +9,19 @@ import static spark.Spark.put;
 
 import com.google.gson.Gson;
 
+import dao.CategoriaDAO;
 import dao.ProdutoDAO;
 import model.Produto;
+import model.Categoria;
 import spark.Filter;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class ApiProduto {
-    
-    private static final ProdutoDAO dao = new ProdutoDAO();
+
+    private static final ProdutoDAO produtoDAO = new ProdutoDAO();
+    private static final CategoriaDAO categoriaDAO = new CategoriaDAO();
     private static final Gson gson = new Gson();
 
     private static final String APPLICATION_JSON = "application/json";
@@ -26,34 +29,34 @@ public class ApiProduto {
     public static void main(String[] args) {
         port(4567);
 
-        after(new Filter(){
+        after(new Filter() {
             @Override
-            public void handle(Request request, Response response){
+            public void handle(Request request, Response response) {
                 response.type(APPLICATION_JSON);
 
             }
         });
 
         // GET /produtos - Buscar Todos
-        get("/produtos", new Route(){
+        get("/produtos", new Route() {
             @Override
-            public Object handle(Request request, Response response){
-                return gson.toJson(dao.buscarTodos());
+            public Object handle(Request request, Response response) {
+                return gson.toJson(produtoDAO.buscarTodos());
             }
         });
 
-        //GET /produtos/id
-        get("/produtos/:id", new Route(){
+        // GET /produtos/id
+        get("/produtos/:id", new Route() {
             @Override
-            public Object handle(Request request, Response response){
+            public Object handle(Request request, Response response) {
                 try {
                     Long id = Long.parseLong(request.params(":id"));
 
-                    Produto produto = dao.buscarPorId(id);
+                    Produto produto = produtoDAO.buscarPorId(id);
 
-                    if(produto != null){
+                    if (produto != null) {
                         return gson.toJson(produto);
-                    }else{
+                    } else {
                         response.status(404);
                         return "{\"mensagem\": \"Produto com ID " + id + "  Não encontrado\"}";
                     }
@@ -67,10 +70,10 @@ public class ApiProduto {
         // POST /produtos - Criar novo produto
         post("/produtos", new Route() {
             @Override
-            public Object handle(Request request, Response response){
+            public Object handle(Request request, Response response) {
                 try {
                     Produto novoProduto = gson.fromJson(request.body(), Produto.class);
-                    dao.inserir(novoProduto);
+                    produtoDAO.inserir(novoProduto);
 
                     response.status(201);
                     return gson.toJson(novoProduto);
@@ -82,18 +85,18 @@ public class ApiProduto {
                     e.printStackTrace();
                     return "{\"mensagem\": \"Erro ao cadastrar o produto\"}";
                 }
-                
+
             }
         });
 
-                // PUT /produtos/:id - Atualizar produto existente
+        // PUT /produtos/:id - Atualizar produto existente
         put("/produtos/:id", new Route() {
             @Override
             public Object handle(Request request, Response response) {
                 try {
                     Long id = Long.parseLong(request.params(":id")); // Usa Long
 
-                    if (dao.buscarPorId(id) == null) {
+                    if (produtoDAO.buscarPorId(id) == null) {
                         response.status(404);
                         return "{\"mensagem\": \"Produto não encontrado para atualização.\"}";
                     }
@@ -101,7 +104,7 @@ public class ApiProduto {
                     Produto produtoParaAtualizar = gson.fromJson(request.body(), Produto.class);
                     produtoParaAtualizar.setId(id); // garante que o ID da URL seja usado
 
-                    dao.atualizar(produtoParaAtualizar);
+                    produtoDAO.atualizar(produtoParaAtualizar);
 
                     response.status(200); // OK
                     return gson.toJson(produtoParaAtualizar);
@@ -125,12 +128,12 @@ public class ApiProduto {
                 try {
                     Long id = Long.parseLong(request.params(":id")); // Usa Long
 
-                    if (dao.buscarPorId(id) == null) {
+                    if (produtoDAO.buscarPorId(id) == null) {
                         response.status(404);
                         return "{\"mensagem\": \"Produto não encontrado para exclusão.\"}";
                     }
 
-                    dao.deletar(id); // Usa o Long ID
+                    produtoDAO.deletar(id); // Usa o Long ID
 
                     response.status(204); // No Content
                     return ""; // Corpo vazio
@@ -138,6 +141,117 @@ public class ApiProduto {
                 } catch (NumberFormatException e) {
                     response.status(400);
                     return "{\"mensagem\": \"Formato de ID inválido.\"}";
+                }
+            }
+        });
+
+        // ----------------------------------------------------------------------------------------------
+        // APIs Categoria
+        // ----------------------------------------------------------------------------------------------
+
+        // get /categorias - Buscar todas as categorias
+        get("/categorias", (request, response) -> {
+            return gson.toJson(categoriaDAO.buscarTodos());
+        });
+
+        // GET /categorias/id
+        get("/categorias/:id", (request, response) -> {
+            try {
+                Long id = Long.parseLong(request.params(":id"));
+
+                Categoria categoria = categoriaDAO.buscarPorId(id);
+
+                if (categoria != null) {
+                    return gson.toJson(categoria);
+                } else {
+                    response.status(404);
+                    return "{\"mensagem\": \"Categoria com ID " + id + "  Não encontrado\"}";
+                }
+            } catch (NumberFormatException e) {
+                response.status(400);
+                return "{\"mensagem\": \"Id Invalido\"}";
+            }
+
+        });
+
+        // POST /categorias - Criar nova categoria
+        post("/categorias", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                try {
+                    Categoria novaCategoria = gson.fromJson(request.body(), Categoria.class);
+                    categoriaDAO.inserir(novaCategoria);
+
+                    response.status(201);
+                    return gson.toJson(novaCategoria);
+
+                } catch (Exception e) {
+                    response.status(500);
+                    System.out.println("Erro ao processar requisição POST /categorias");
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                    return "{\"mensagem\": \"Erro ao cadastrar a categoria.\"}";
+                }
+            }
+        });
+
+        // PUT /categorias/:id - Atualizar categoria existente
+        put("/categorias/:id", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                try {
+                    Long id = Long.parseLong(request.params(":id"));
+
+                    if (categoriaDAO.buscarPorId(id) == null) {
+                        response.status(404);
+                        return "{\"mensagem\": \"Categoria não encontrada para atualização.\"}";
+                    }
+
+                    Categoria categoriaParaAtualizar = gson.fromJson(request.body(), Categoria.class);
+                    categoriaParaAtualizar.setId(id);
+
+                    categoriaDAO.atualizar(categoriaParaAtualizar);
+
+                    response.status(200); // OK
+                    return gson.toJson(categoriaParaAtualizar);
+
+                } catch (NumberFormatException e) {
+                    response.status(400); // Bad Request
+                    return "{\"mensagem\": \"Formato de ID inválido.\"}";
+                } catch (Exception e) {
+                    response.status(500);
+                    System.err.println("Erro ao processar requisição PUT /categorias: " + e.getMessage());
+                    e.printStackTrace();
+                    return "{\"mensagem\": \"Erro ao atualizar a categoria.\"}";
+                }
+            }
+        });
+
+        // DELETE /categorias/:id - Deletar uma categoria
+        delete("/categorias/:id", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                try {
+                    Long id = Long.parseLong(request.params(":id"));
+
+                    if (categoriaDAO.buscarPorId(id) == null) {
+                        response.status(404);
+                        return "{\"mensagem\": \"Categoria não encontrada para exclusão.\"}";
+                    }
+
+                    categoriaDAO.deletar(id);
+
+                    response.status(204); // No Content
+                    return ""; // Corpo vazio
+
+                } catch (NumberFormatException e) {
+                    response.status(400);
+                    return "{\"mensagem\": \"Formato de ID inválido.\"}";
+                } catch (Exception e) {
+                    response.status(500);
+                    System.err.println("Erro ao processar requisição DELETE /categorias: " + e.getMessage());
+                    e.printStackTrace();
+                    return "{\"mensagem\": \"Erro ao excluir a categoria.\"}";
                 }
             }
         });
--- src/dao/ProdutoDAO.java
+++ src/dao/ProdutoDAO.java
@@ -8,8 +8,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.mysql.cj.xdevapi.Schema.CreateCollectionOptions;
-
 import model.Produto;
 import util.ConnectionFactory;
 
